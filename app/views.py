b72d101d0cbac2a1aeee86bb9fc15772a86ef12c
islombek-developer <164884102+islombek-developer@users.noreply.github.com>
2024-07-14 16:58:59 +0300

    dsa

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+
+from .models import Video
+from .serializers import VideoSerializer
+from .permissions import VideoPermission
+
+class VideoCreateView(generics.ListCreateAPIView, generics.ListAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+    permission_classes = [VideoPermission]
+
+
+class VideoUpdateView(generics.RetrieveUpdateAPIView, generics.RetrieveUpdateDestroyAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+    permission_classes = [VideoPermission]
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from app.views import VideoCreateView, VideoUpdateView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/v1/video/', VideoCreateView.as_view()),
+    path('api/v1/video/<int:pk>/', VideoUpdateView.as_view()),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    path('api-auth/', include('rest_framework.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
